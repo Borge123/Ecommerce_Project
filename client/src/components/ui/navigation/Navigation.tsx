a352eaed82ff5aa28f145f10f14cede683af48cc
Thomas Borge <thomas.borge@fborge.no>
2023-10-20 13:29:29 +0200

    added another navbar with logo and searchbar and styled it + made some changes to other navbar

--- client/src/components/ui/navigation/Navigation.tsx
+++ client/src/components/ui/navigation/Navigation.tsx
@@ -1,4 +1,6 @@
 import { useEffect, useState } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
@@ -11,125 +13,138 @@ export default function Navigation() {
   const authState = useUser();
 
   return (
-    <Navbar
-      collapseOnSelect
-      expand="lg"
-      className="bg-body-tertiary pb-4"
-      data-bs-theme="auto"
-    >
-      <Container>
-        <Navbar.Brand as={Link} to="/">
-          React-Bootstrap
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link
-              as={NavLink}
-              to="/"
-              style={({ isActive, isPending }) => {
-                return {
-                  fontWeight: isActive ? "bold" : "",
-                  color: isPending ? "red" : "black",
-                };
-              }}
-            >
-              Home
-            </Nav.Link>
-            <Nav.Link
-              as={NavLink}
-              to="/products"
-              style={({ isActive, isPending }) => {
-                return {
-                  fontWeight: isActive ? "bold" : "",
-                  color: isPending ? "red" : "black",
-                };
-              }}
-            >
-              Products
-            </Nav.Link>
+    <header>
+      <Navbar expand="lg" className="bg-body-tertiary">
+        <Container className="">
+          <Nav className="">
+            <Navbar.Brand as={Link} to="/">
+              Navbar scroll
+            </Navbar.Brand>
           </Nav>
-          <Nav>
-            <Nav.Link
-              as={NavLink}
-              to="/signup"
-              style={({ isActive, isPending }) => {
-                return {
-                  fontWeight: isActive ? "bold" : "",
-                  color: isPending ? "red" : "black",
-                };
-              }}
-            >
-              Signup
-            </Nav.Link>
-          </Nav>
-          {!authState.user ? (
-            <Nav>
+
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll" style={{ flex: "0 1 900px" }}>
+            <Nav style={{ flex: "1 1 100%" }}>
+              <Form className="d-flex w-100">
+                <Form.Control
+                  type="search"
+                  placeholder="Search"
+                  className="me-2 w-100"
+                  aria-label="Search"
+                />
+              </Form>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <Navbar
+        collapseOnSelect
+        expand="lg"
+        className="bg-body-tertiary pb-4"
+        data-bs-theme="auto"
+      >
+        <Container>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="">
               <Nav.Link
                 as={NavLink}
-                to="/login"
+                to="/"
                 style={({ isActive, isPending }) => {
                   return {
-                    fontWeight: isActive ? "bold" : "normal",
+                    fontWeight: isActive ? "bold" : "",
                     color: isPending ? "red" : "black",
                   };
                 }}
               >
-                Login
+                Home
               </Nav.Link>
-            </Nav>
-          ) : (
-            <Nav>
-              <NavDropdown
-                title={authState?.user.firstName}
-                id="collasible-nav-dropdown"
-              >
-                <NavDropdown.Item
-                  as={NavLink}
-                  to="/account"
-                  style={({ isActive, isPending }) => {
-                    return {
-                      fontWeight: isActive ? "bold" : "normal",
-                      color: isPending ? "red" : "black",
-                      backgroundColor: isActive ? "inherit" : "inherit",
-                    };
-                  }}
-                >
-                  Account
-                </NavDropdown.Item>
-                <NavDropdown.Item
-                  as={NavLink}
-                  to="/"
-                  style={({ isActive, isPending }) => {
-                    return {
-                      fontWeight: isActive ? "bold" : "normal",
-                      color: isPending ? "red" : "black",
-                      backgroundColor: isActive ? "inherit" : "inherit",
-                    };
-                  }}
-                >
-                  Orders
-                </NavDropdown.Item>
-                <NavDropdown.Item href="">Something</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <Logout click={logout} />{" "}
-              </NavDropdown>
               <Nav.Link
                 as={NavLink}
-                to="/account"
+                to="/products"
                 style={({ isActive, isPending }) => {
                   return {
-                    fontWeight: isActive ? "bold" : "normal",
+                    fontWeight: isActive ? "bold" : "",
                     color: isPending ? "red" : "black",
                   };
                 }}
               >
-                {authState.user.firstName}
+                Products
               </Nav.Link>
             </Nav>
-          )}
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+            <Nav>
+              <Nav.Link
+                as={NavLink}
+                to="/signup"
+                style={({ isActive, isPending }) => {
+                  return {
+                    fontWeight: isActive ? "bold" : "",
+                    color: isPending ? "red" : "black",
+                  };
+                }}
+              >
+                Signup
+              </Nav.Link>
+            </Nav>
+            {!authState.user ? (
+              <Nav>
+                <Nav.Link
+                  as={NavLink}
+                  to="/login"
+                  style={({ isActive, isPending }) => {
+                    return {
+                      fontWeight: isActive ? "bold" : "normal",
+                      color: isPending ? "red" : "black",
+                    };
+                  }}
+                >
+                  Login
+                </Nav.Link>
+              </Nav>
+            ) : (
+              <Nav>
+                <NavDropdown
+                  title={authState?.user.firstName}
+                  id="collasible-nav-dropdown"
+                >
+                  <NavDropdown.Item
+                    as={NavLink}
+                    to="/account"
+                    style={({ isActive, isPending }) => {
+                      return {
+                        fontWeight: isActive ? "bold" : "normal",
+                        color: isPending ? "red" : "black",
+                        backgroundColor: isActive ? "inherit" : "inherit",
+                      };
+                    }}
+                  >
+                    Account
+                  </NavDropdown.Item>
+                  <NavDropdown.Item
+                    as={NavLink}
+                    to="/"
+                    style={({ isActive, isPending }) => {
+                      return {
+                        fontWeight: isActive ? "bold" : "normal",
+                        color: isPending ? "red" : "black",
+                        backgroundColor: isActive ? "inherit" : "inherit",
+                      };
+                    }}
+                  >
+                    Orders
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href="">Something</NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <Logout click={logout} />{" "}
+                </NavDropdown>
+                <Navbar.Text>
+                  Signed in as: {authState.user.firstName}
+                </Navbar.Text>
+              </Nav>
+            )}
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
   );
 }
