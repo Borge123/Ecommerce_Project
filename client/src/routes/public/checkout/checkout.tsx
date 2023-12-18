9ddfc2d75aecc0ff5dd4e6c6ddaf23c60602cf86
Thomas Borge <thomas.borge@fborge.no>
2023-12-18 15:27:21 +0100

    added child elements to checkout route and added outlet to be able to load the right components for the right stage in the checkout proccess

--- client/src/routes/public/checkout/checkout.tsx
+++ client/src/routes/public/checkout/checkout.tsx
@@ -1,7 +1,20 @@
 import { CheckoutLoader } from "./checkoutLoader";
+import { CheckoutForm } from "../../../features/checkout/components/checkoutform";
 import CheckoutPage from "../../../pages/CheckoutPage";
 export const checkout = {
   path: "checkout",
   element: <CheckoutPage />,
   //loader: CheckoutLoader,
+
+  children: [
+    {
+      path: "/checkout/summary",
+      element: <h1>Cart summary</h1>,
+    },
+
+    {
+      path: "/checkout/form",
+      element: <CheckoutForm />,
+    },
+  ],
 };
