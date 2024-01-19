8b4824d5944eef5ca1def8bfbeb7a811cd827cad
Thomas Borge <thomas.borge@fborge.no>
2024-01-19 13:30:22 +0100

    made some progress on the table

--- client/src/features/adminDashboard/components/table.tsx
+++ client/src/features/adminDashboard/components/table.tsx
@@ -3,33 +3,41 @@ import Table from "react-bootstrap/Table";
 export function AdminTable({ headers, data }) {
   //TODO make it so that every component who uses the table have to construct its own headers
   //atm every single key from the data objects are set as headers which is unneccesary
+  console.log(data);
+
   return (
     <Table responsive>
       <thead>
         <tr>
-          <th>#</th>
-
-          {Object.keys(data[0]).map((header, index) => {
+          {headers.map((header) => {
             return (
-              <td key={index}>
-                <p>{header}</p>
-              </td>
+              <th scope="col" key={header}>
+                {header}
+              </th>
             );
           })}
         </tr>
       </thead>
       <tbody>
-        {/* {data.map((datum, index) => {
+        {data.map((datum, index) => {
           return (
             <tr key={datum._id}>
-              {Object.keys(data[0]).map((header, index) => {
-                <td key={index}>
-                  <span>{datum[header]}</span>
-                </td>;
-              })}
+              {datum.billinginfo ? (
+                //TODO figure out a better way to deal with nested objects
+                <>
+                  <td>{datum.billinginfo?.address}</td>
+                  <td>{datum.billinginfo?.city}</td>
+                  <td>{datum.billinginfo?.house_number}</td>
+                  <td>{datum.billinginfo?.zip}</td>
+                </>
+              ) : (
+                <td>Empty</td>
+              )}
+
+              <td>{datum._id}</td>
             </tr>
           );
-        })} */}
+        })}
       </tbody>
     </Table>
   );
--- client/src/features/adminDashboard/components/users.tsx
+++ client/src/features/adminDashboard/components/users.tsx
@@ -3,11 +3,13 @@ import { Col } from "react-bootstrap";
 import { AdminTable } from "./table";
 export function Users() {
   const { users } = useLoaderData();
+  const headers = Object.keys(users[0]);
+  //console.log(users);
 
   return (
     <Col>
       <h1>Users</h1>
-      <AdminTable headers={{ testkey: "test" }} data={users}></AdminTable>
+      <AdminTable headers={headers} data={users}></AdminTable>
     </Col>
   );
 }
